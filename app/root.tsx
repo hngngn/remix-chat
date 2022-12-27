0a77c9ea8057618d124ddafd10c05b878298a6be
hnggngn <rozephyrus@gmail.com>
2022-12-27 21:44:58 +0700

    feat: setup unocss

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,34 @@
-import type { MetaFunction } from "@remix-run/node"
+import type { LinksFunction, MetaFunction } from "@remix-run/node"
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
+import reset from "@unocss/reset/tailwind.css"
+import custom from "~/assets/styles/custom.css"
+import satoshi from "~/assets/styles/satoshi.css"
+import uno from "../styles/uno.css"
 
 export const meta: MetaFunction = () => ({
     charset: "utf-8",
     title: "New Remix App",
     viewport: "width=device-width,initial-scale=1",
 })
+
+export const links: LinksFunction = () => [
+    {
+        rel: "stylesheet",
+        href: reset,
+    },
+    {
+        rel: "stylesheet",
+        href: uno,
+    },
+    {
+        rel: "stylesheet",
+        href: satoshi,
+    },
+    {
+        rel: "stylesheet",
+        href: custom,
+    },
+]
 
 const App = () => {
     return (
